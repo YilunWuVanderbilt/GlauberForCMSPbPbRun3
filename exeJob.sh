e7ba52837a3d3a3c04609ab89d6ae19cb1af154e
Yilun Wu <106136747+YilunWuVanderbilt@users.noreply.github.com>
2024-05-22 21:48:34 -0500

    Update exeJob.sh

--- exeJob.sh
+++ exeJob.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 export SCRAM_ARCH=el8_amd64_gcc11
-cd /afs/cern.ch/user/y/yilun/CMSSW_13_0_3/src 
+cd /location/of/any/CMSSW/version/you/have/on/lxplus/src
 #cmsenv
 eval `scramv1 runtime -sh`
 
-#path="/afs/cern.ch/user/t/tuos/work/private/Glauber2016/version3/code/v31/pbpb/default/5360"
-#jbi=default
-#cd $path/$jbi 
-cd /afs/cern.ch/user/y/yilun/GlauberModel/default
+cd /current/directory/where/you/put/this/glauber/code
 
 $ROOTSYS/bin/root -l -b <<EOF
 {
   gSystem->Load("libMathMore");
-  gROOT->ProcessLine(".L /afs/cern.ch/user/y/yilun/GlauberModel/default/runglauber_v3.1.C+");
+  gROOT->ProcessLine(".L /current/directory/where/you/put/this/glauber/code/runglauber_v3.1.C+");
   runAndSaveNtuple(1000000,"Pbpnrw","Pbpnrw",68.0,-1,0.4,-1,-1,"glauber_PbpnrwPbpnrw_default_1M.root");
 }
 EOF
